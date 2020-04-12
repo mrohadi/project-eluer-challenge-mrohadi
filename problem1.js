6f11630cfae5dd620c99f167dfa632fbae097bc2
DeBujos <55712332+mrohadi@users.noreply.github.com>
2020-04-12 18:52:14 +0700

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,9 +1,9 @@
 //Function Declaration
-    var total;
-            total = 0;
+var total;
+total = 0;
 
-            for (let i = 0; i < 1000; i++){
-                if (i % 3 == 0 || i % 5 == 0){
-                    total += i
-                }
-            }
+for (let i = 0; i < 1000; i++){
+    if (i % 3 == 0 || i % 5 == 0){
+        total += i
+    }
+}
