2976d88dac1e601b3921d570c5b860e7c91200e5
DeBujos <55712332+mrohadi@users.noreply.github.com>
2020-04-12 18:50:46 +0700

    Create problem1.js

---
+++ problem1.js
@@ -0,0 +1,9 @@
+//Function Declaration
+    var total;
+            total = 0;
+
+            for (let i = 0; i < 1000; i++){
+                if (i % 3 == 0 || i % 5 == 0){
+                    total += i
+                }
+            }
