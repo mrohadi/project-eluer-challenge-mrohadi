da719f6b29cdb4a4aee610f81792431d39eae8f5
DeBujos <55712332+mrohadi@users.noreply.github.com>
2020-04-12 19:59:58 +0700

    Update problem1.js

--- Project Euler/problem1.js
+++ Project Euler/problem1.js
@@ -1,9 +1,14 @@
-//Function Declaration
-var total;
-total = 0;
-
-for (let i = 0; i < 1000; i++){
-    if (i % 3 == 0 || i % 5 == 0){
-        total += i
-    }
-}
+//Function Declaration of multiple of 3 and 5 problem
+function sumOfMultiple(num){
+    var total;
+    total = 0;
+    //Loop through the num 
+    for (let i = 0; i < num; i++){
+        if (i % 3 == 0 || i % 5 == 0){
+            total += i;
+         };
+     };
+     return total;
+};
+//Now let's try our function through var test
+var test = sumOfMultiple(10000);
